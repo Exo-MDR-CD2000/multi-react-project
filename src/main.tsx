0090425016def5613599e54ce7f83c51b89aaa25
JAG24_Exo <joseguillen587@yahoo.com>
2024-10-09 14:41:38 -0700

    testing bootstrap imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './components/App'
+
+
+// Import Dependencies
+// import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+// import '@fortawesome/fontawesome-free/css/all.min.css'; // Import FontAwesome CSS
+
+// Styles
 import './css/index.css'
 
 createRoot(document.getElementById('root')!).render(
