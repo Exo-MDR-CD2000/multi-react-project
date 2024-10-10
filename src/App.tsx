3e9f58aac1377aa677b7e10c42e87d7997d23dab
JAG24_Exo <joseguillen587@yahoo.com>
2024-10-10 14:31:38 -0700

    combined 2 imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import { useState } from 'react'
-import React from 'react';
+import React, { useState } from 'react';
 // import reactLogo from '../assets/react.svg'
 // import viteLogo from '/vite.svg'
 import { initialRecipes, Recipe } from './data/recipes'; // Import the Recipe type
