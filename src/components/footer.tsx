48554a536f1da3b386e808d66691d503e8468777
JAG24_Exo <joseguillen587@yahoo.com>
2024-10-10 14:23:25 -0700

    added template files for footer, about, home, and contact

---
+++ src/components/footer.tsx
@@ -0,0 +1,1 @@
+// create bootstrap footer here
---
+++ src/pages/aboutPage.tsx
@@ -0,0 +1,8 @@
+// src/pages/AboutPage.tsx
+import React from 'react';
+
+const AboutPage: React.FC = () => {
+  return <div>About Page</div>;
+};
+
+export default AboutPage;
