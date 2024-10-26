8e17ffe89a252e00c801685c6e5963e011c75c03
JAG24_Exo <joseguillen587@yahoo.com>
2024-10-26 16:48:42 -0700

    added helper function for current year footer

---
+++ src/utilities/footerCurrentYear.ts
@@ -0,0 +1,11 @@
+// create helper function that returns the current year for the footer.
+
+/**
+ * Helper function that returns the current year for the footer.
+ * @returns {number} The current year.
+ */
+const FooterCurrentYear = () => {
+    return new Date().getFullYear();
+};
+
+export default FooterCurrentYear;
