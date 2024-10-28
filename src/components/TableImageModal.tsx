b7cc68578182bda392b99b770c1fa5d84466c9b2
JAG24_Exo <joseguillen587@yahoo.com>
2024-10-28 15:01:24 -0700

    added backdrop to image modal jsx

--- src/components/TableImageModal.tsx
+++ src/components/TableImageModal.tsx
@@ -9,22 +9,29 @@ interface ImageModalProps {
 
 const ImageModal: React.FC<ImageModalProps> = ({ show, imageUrl, recipeTitle, onClose }) => {
   return (
-    <div className={`modal fade ${show ? 'show d-block' : ''}`} tabIndex={-1} role="dialog">
-      <div className="modal-dialog modal-dialog-centered" role="document">
-        <div className="modal-content bg-light">
-          <div className="modal-header justify-content-between">
-            <h5 className="modal-title">{recipeTitle}</h5>
-            <button type="button" className="btn btn-danger btn-sm" aria-label="Close" onClick={onClose}>
-            <i className="bi bi-x"></i>
-            </button>
-          </div>
-          <div className="modal-body">
-            <img src={imageUrl} alt="Recipe" className="img-fluid" />
+    <>
+      {/* Backdrop overlay */}
+      <div className={`backdrop ${show ? 'show' : ''}`} onClick={onClose}></div>
+
+      {/* Modal content */}
+      <div className={`modal fade ${show ? 'show d-block' : ''}`} tabIndex={-1} role="dialog">
+        <div className="modal-dialog modal-dialog-centered" role="document">
+          <div className="modal-content bg-light">
+            <div className="modal-header justify-content-between">
+              <h5 className="modal-title">{recipeTitle}</h5>
+              <button type="button" className="btn btn-danger btn-sm" aria-label="Close" onClick={onClose}>
+                <i className="bi bi-x"></i>
+              </button>
+            </div>
+            <div className="modal-body">
+              <img src={imageUrl} alt="Recipe" className="img-fluid" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
+
 
 export default ImageModal;
