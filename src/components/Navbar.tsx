12e498de6a43d9d42ac88da057fee428438592fd
JAG24_Exo <joseguillen587@yahoo.com>
2024-10-27 23:40:01 -0700

    renamed navbar brand

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar: React.FC = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">My Recipe App</Link>
+        <Link className="navbar-brand" to="/">Recipe Manager</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
