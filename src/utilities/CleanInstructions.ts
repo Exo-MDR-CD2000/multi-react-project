8fb7e051439822e26e2c9c780e9c470ce1f8ed5c
JAG24_Exo <joseguillen587@yahoo.com>
2024-10-29 16:04:27 -0700

    removed unused utility helper

--- src/utilities/CleanInstructions.ts
+++
@@ -1,3 +0,0 @@
-export const cleanInstructions = (instructions: string): string[] => {
-  return instructions.split('. ').filter(step => step.trim() !== '').map(step => step.replace(/^\d+\.\s*/, ''));
-};
