d396d8c949396aa6ff129fb1ea44dae8fa03ed9c
JAG24_Exo <joseguillen587@yahoo.com>
2024-10-29 13:23:26 -0700

    created layout component for proper react router implementation

---
+++ src/components/Layout.tsx
@@ -0,0 +1,20 @@
+// import React from 'react';
+// import { Outlet } from 'react-router-dom';
+// import Navbar from './components/Navbar';
+// import Footer from './components/Footer';
+// import ScrollBackToTop from './components/ScrollBackToTop';
+
+// const Layout: React.FC = () => {
+//   return (
+//     <div className="d-flex flex-column">
+//       <Navbar />
+//       <div className="container mt-4">
+//         <Outlet /> {/* This is where child routes will be rendered */}
+//         <ScrollBackToTop />
+//       </div>
+//       <Footer />
+//     </div>
+//   );
+// };
+
+// export default Layout;
